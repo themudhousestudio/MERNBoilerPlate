3a3f29a022540f701b35f59cea02a5f62e3e8e84
Ahmed Faraz <themudhousestudio@gmail.com>
2020-08-09 13:04:06 -0700

    initialize heroku depoloyment

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://ahmedfaraz:Avny1yBQ7fTR5Xmw@ecomdb.x9y5n.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://ahmedfaraz:xLolKdwQyxBioPex@ecomdb.x9y5n.mongodb.net/<dbname>?retryWrites=true&w=majority'
 }
+//119.63.139.125/32
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const {User} = require('./models/user');
 const config = require('./config/key');
 const psw = '4QUPKIfJaEYFHgyR';
+const {auth} = require('./middleware/auth')
 mongoose.connect(config.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log("DB Connected"))
 .catch(err => console.error(err));
@@ -16,7 +17,19 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
 app.use(cookieParser());
 
-app.post('/api/users/register', (req,res) => {
+
+app.get("/api/users/auth", auth, (req, res) =>{
+    res.status(200).json({
+        _id:req._id,
+        isAuth:true,
+        email:req.user.email,
+        name:req.user.name,
+        lastname:req.user.lastname,
+        role: req.user.role
+    })
+})
+
+app.post("/api/users/register", (req,res) => {
     const user = new User(req.body);
 
     user.save((err,userData)=> {
@@ -25,11 +38,43 @@ app.post('/api/users/register', (req,res) => {
             success:true
         })
     })
-    
 })
 
-const port = 5000
+app.post("/api/user/signin", (req, res)=>{
+        // find email
+    User.findOne({email: req.body.email}, (err, user) =>{
+        if(!user) return res.json({
+            loginSuccess: false,
+            message: "Auth Failed, email not found"
+        });
+        //compare password
 
-app.get('/', (req, res) => res.send('Hello World!'))
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch){
+                return res.json({loginSuccess:false, message:"wrong password"})
+            }
+        })
+        //generate token
+        user.generateToken((err, user)=>{
+             if(err) return res.status(400).send(err);
+             res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true
+                })
+        })
+    })
+})
+
+app.get("/api/users/logout", auth, (req, res) =>{
+    User.findOneAndUpdate({_id:req.user._id}, {token:""}, (err, doc) =>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).send({
+            success: true 
+        })
+    })
+})
+
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
